f2e34be565631626ea09c0c482f96d437f2011d0
Brinda Bhattarai <brindaabrindaa7@gmail.com>
2024-05-17 21:59:42 +0545

    made some auth screens

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:walkwise/core/utils/asset_provider.dart';
+import 'package:walkwise/core/utils/util.dart';
+import 'package:walkwise/screen/login_screen.dart';
+import 'package:walkwise/widgets/responsive_text.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(
+            horizontal: kHorizontalMargin, vertical: kVerticalMargin),
+        child: Column(children: [
+          Image.asset(Assets.images.Logo),
+          Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: kVerticalMargin),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(width * 2, height * 0.07),
+                  backgroundColor: Color(0xFF614E7E),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: ResponsiveText(
+                  "Get Started",
+                  fontSize: 24,
+                  fontFamily: 'Times',
+                  fontWeight: FontWeight.w400,
+                  textColor: Color(0xFFFCFCFC),
+                ),
+                onPressed: () {
+                  {
+                    MaterialPageRoute(
+                      builder: (context) => LoginScreen(),
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
